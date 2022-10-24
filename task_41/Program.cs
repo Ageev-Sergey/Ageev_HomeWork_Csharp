a4c934851e06da4a933c4f7631cdb8704e794aa5
Sergey <sergey_ageev13@mail.ru>
2022-10-24 17:23:00 +0300

    addded task 41,43

---
+++ task_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string myString = NumberString();
+int sumValuyРigherZero = AboveZero(myString);
+Console.WriteLine(myString);
+Console.WriteLine(sumValuyРigherZero);
+
+
+string NumberString()
+{
+    string a = "";
+    string newString = "";
+    do
+    {
+        Console.WriteLine("Введите число, или нажмите Enter  для выхода: ");
+        a = Console.ReadLine();
+        newString = newString + a + " ";
+    }
+    while (a != "");
+    return newString;
+}
+
+int AboveZero(string thisString)
+{
+    int a;
+    int sum = 0;
+    string[] myStringArray = myString.Split(" ");
+    for (int i = 0; i < myStringArray.Length; i++)
+    {
+        if (
+            int.TryParse(myStringArray[i], out a)
+            && a > 0
+            )
+            sum++;
+    }
+    return sum;
+}
---
+++ task_43/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+double[] firstLine = NewLine();
+double[] secondLine = NewLine();
+double[] intersectionPoint = IsIntersectionPoint(firstLine, secondLine);
+
+Console.WriteLine($"первая линия: y = {firstLine[0]}x + {firstLine[1]} ");
+Console.WriteLine($"вторя линия: y = {secondLine[0]}x + {secondLine[1]}");
+Console.WriteLine("точка пересечения "+string.Join(", ", intersectionPoint));
+
+
+
+double[] NewLine()
+{
+    double a;
+    double c;
+    bool check = false;
+    do
+    {
+        Console.WriteLine("Введите коэффициент: ");
+        check = Double.TryParse(Console.ReadLine(), out a);
+        if (check == false) Console.WriteLine("некорректное значение коэффициента!");
+
+    }
+    while (check == false);
+
+    do
+    {
+        Console.WriteLine("Введите С: ");
+        check = Double.TryParse(Console.ReadLine(), out c);
+        if (check == false) Console.WriteLine("некорректное значение С!");
+    }
+    while (check == false);
+
+    double[] newLine = new double[2];
+    newLine[0] = a;
+    newLine[1] = c;
+    return newLine;
+}
+
+double[] IsIntersectionPoint(double[] first,double[] second)
+{
+    double x = (first[0]-second[0])/(second[1]-first[1]);
+    double y = first[0]*x + first[1];
+    double[] point = new double [2]{x, y};
+    return point;
+}
