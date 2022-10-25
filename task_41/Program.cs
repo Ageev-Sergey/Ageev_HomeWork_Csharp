7def327d310cd1077302599b6d94b798bf939ae9
Sergey <sergey_ageev13@mail.ru>
2022-10-25 15:49:10 +0300

    added change task 41 43

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -1,4 +1,5 @@
-﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿/*
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 string myString = NumberString();
 int sumValuyРigherZero = AboveZero(myString);
@@ -25,6 +26,38 @@ int AboveZero(string thisString)
     int a;
     int sum = 0;
     string[] myStringArray = myString.Split(" ");
+    for (int i = 0; i < myStringArray.Length; i++)
+    {
+        if (
+            int.TryParse(myStringArray[i], out a)
+            && a > 0
+            )
+            sum++;
+    }
+    return sum;
+}
+
+ */
+
+
+string myString = NumberString();
+int sumValuyРigherZero = AboveZero(myString);
+Console.WriteLine(myString);
+Console.WriteLine(sumValuyРigherZero);
+
+
+string NumberString()
+{
+    Console.WriteLine("Введите числа через запятую: ");
+    string newString = Console.ReadLine();
+    return newString;
+}
+
+int AboveZero(string thisString)
+{
+    int a;
+    int sum = 0;
+    string[] myStringArray = myString.Split(",");
     for (int i = 0; i < myStringArray.Length; i++)
     {
         if (
--- task_43/Program.cs
+++ task_43/Program.cs
@@ -1,4 +1,5 @@
-﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+﻿/*
+// Напишите программу, которая найдёт точку пересечения двух прямых,
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
 // значения b1, k1, b2 и k2 задаются пользователем.
 
@@ -35,9 +36,7 @@ double[] NewLine()
     }
     while (check == false);
 
-    double[] newLine = new double[2];
-    newLine[0] = a;
-    newLine[1] = c;
+    double[] newLine = new double[2]{a,c};
     return newLine;
 }
 
@@ -45,6 +44,55 @@ double[] IsIntersectionPoint(double[] first,double[] second)
 {
     double x = (first[0]-second[0])/(second[1]-first[1]);
     double y = first[0]*x + first[1];
+    double[] point = new double [2]{x, y};
+    return point;
+}
+ */
+
+
+double k1 = NewValue("введите k1: ");
+double b1 = NewValue("введите b1: ");
+double k2 = NewValue("введите k2: ");
+double b2 = NewValue("введите b2: ");
+double[] firstLine = NewLine(k1,b1);
+double[] secondLine = NewLine(k2,b2);
+double intersectionPointX = IsIntersectionPointX(firstLine, secondLine);
+double intersectionPointY = IsIntersectionPointY(intersectionPointX);
+double[] intersectionPointXY = IsIntersectionPointXY(intersectionPointX, intersectionPointY);
+
+Console.WriteLine($"первая линия: y = {firstLine[0]}x + {firstLine[1]} ");
+Console.WriteLine($"вторя линия: y = {secondLine[0]}x + {secondLine[1]}");
+Console.WriteLine("точка пересечения "+string.Join(", ", intersectionPointXY));
+
+
+
+double NewValue(string message)
+{
+    Console.WriteLine(message);
+    double a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+double [] NewLine (double k, double b)
+{
+    double[] line = new double [2]{k,b};
+    return line;
+}
+
+double IsIntersectionPointX(double[] first,double[] second)
+{
+    double x = (first[0]-second[0])/(second[1]-first[1]);
+    return x;
+}
+
+double IsIntersectionPointY(double x)
+{
+    double y = k1*x + b1;
+    return y;
+}
+
+double[] IsIntersectionPointXY(double x,double y)
+{
     double[] point = new double [2]{x, y};
     return point;
 }
