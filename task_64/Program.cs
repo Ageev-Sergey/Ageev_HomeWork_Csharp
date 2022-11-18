ad9ba6e95aa29725dfd8e145637d6f5c15d86ab5
Sergey <sergey_ageev13@mail.ru>
2022-11-18 16:34:41 +0300

    addded task 64 66 68

---
+++ task_64/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// // N = 5 -> "5, 4, 3, 2, 1"
+// // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int N = IsValue("Введите число N");
+AllValue(N);
+
+int IsValue(string massege)
+{
+    Console.WriteLine(massege);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+void AllValue(int N)
+{
+    if (N > 1)
+    {
+        Console.Write($"{N}, ");
+        AllValue(N - 1);
+    }
+    else if (N == 1) Console.Write(N);
+    else Console.WriteLine("недопустимое значение, число должной быть больше 0");
+}
---
+++ task_66/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int N = IsValue("Введите число N");
+int M = IsValue("Введите число M");
+int sum = SumAllValue(N, M);
+Console.WriteLine(sum);
+
+int IsValue(string massege)
+{
+    Console.WriteLine(massege);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int SumAllValue(int one, int two, int sum = 0)
+{
+    if (N != M)
+    {
+        if (one > two)
+        {
+            sum += one;
+            sum = SumAllValue(--one, two, sum);
+        }
+        else if (two > one)
+        {
+            sum += one;
+            sum = SumAllValue(++one, two, sum);
+        }
+        else sum += one;
+        return sum;
+    }
+    Console.WriteLine("числа равны");
+    return sum;
+}
